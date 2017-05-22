ed52f9bd9092cbe2387d7cea4120c38aebe26efd
tilakpatidar <tilakpatidar@gmail.com>
2017-05-22 21:45:27 +0530

    Add cats demo

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "exceptions_in_scala"
 version := "1.0"
 
 scalaVersion := "2.12.2"
-        
+
+libraryDependencies += "org.typelevel" %% "cats" % "0.9.0"
---
+++ src/main/scala/CatsDemo.scala
@@ -0,0 +1,17 @@
+import cats.implicits._
+
+import scala.util.{Failure, Success, Try}
+
+object CatsDemo extends App {
+
+  override def main(args: Array[String]): Unit = {
+
+    // We have a List[Try[String]] but we actually want a Try[List[String]].
+    // we want it to be a Failure if any of the individual Trys was a Failure.
+    val listOfTries: List[Try[String]] = List(Failure(new Exception), Success("b"), Success("c"))
+
+//    val listOfTries: List[Try[String]] = List(Success("a"), Success("b"), Success("c"))
+    val tryOfList: Try[List[String]] = listOfTries.sequence
+    println(tryOfList)
+  }
+}
